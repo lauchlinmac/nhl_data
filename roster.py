2105be972e761a82f18963c253fc78a44df23ed3
lauchlinmac <lauchlinmac@gmail.com>
2018-09-13 11:56:07 -0400

    Added Team Name

--- roster.py
+++ roster.py
@@ -1,8 +1,9 @@
 import json, urllib.request,requests
 nhl_data = 'https://statsapi.web.nhl.com/api/v1/teams/1/roster'
-
+team_name = 'https://statsapi.web.nhl.com/api/v1/teams/1'
 # This is a good version of getting the roster - need to sort*/
 json_data = requests.get(nhl_data).json()
+team_data = requests.get(team_name).json()
 
 number = len(json_data['roster'])
 i=0
@@ -12,11 +13,11 @@ while i < number:
     players.append(json_data['roster'][i]['person']['fullName'])
     i=i+1
     players.sort()
-
-print('There are',number,'players on the team.')
-print('----------------------------------')
+t_name = team_data['teams'][0]['name']
+print('There are',number,'players on the',t_name)
+print('---------------------------------------')
 i=0
 while i < number:
-    print(players[i])
+    print(players[i]) 
     i=i+1
 
